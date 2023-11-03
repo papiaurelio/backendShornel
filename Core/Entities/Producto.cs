6f425ded48c75c4e3a686c753127a62e454c39b9
Aurelio <byrondereyes@gmail.com>
2023-11-03 02:53:34 -0600

    Primer p

---
+++ Core/Entities/Producto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Producto : ClaseBase
+    {
+
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public int Stock { get; set; }
+
+        //lave f
+        public int MarcaId { get; set; }
+
+        //referencia llave f
+        public Marca Marca { get; set; }
+
+        public int CategoriaId { get; set; }
+
+        public Categoria Categoria { get; set; }
+
+        public decimal Precio { get; set; }
+
+        public string Imagen { get; set; }
+
+    }
+}
---
+++ WebApi/Startup.cs
@@ -0,0 +1,41 @@
+﻿using BusinessLogic.Logic;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WebApi;
+
+public class Startup
+{
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddTransient<IProductoRepository, ProductoRepository>();
+        services.AddControllers();
+    }
+
+    public void Configure(IApplicationBuilder App, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            App.UseDeveloperExceptionPage();
+        }
+
+        App.UseRouting();
+        App.UseAuthentication();
+        App.UseAuthorization();
+        App.UseEndpoints(endpoints => {
+            endpoints.MapControllers();
+        });
+    }
+}
+
