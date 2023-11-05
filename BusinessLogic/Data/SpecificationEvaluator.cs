23f5c5743824344d705d781680682188c2febf50
Aurelio <byrondereyes@gmail.com>
2023-11-04 23:51:03 -0600

    Specifications generic

---
+++ BusinessLogic/Data/SpecificationEvaluator.cs
@@ -0,0 +1,26 @@
+﻿using Core.Entities;
+using Core.Specifications;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Data
+{
+    public class SpecificationEvaluator<T> where T : ClaseBase
+    {
+        public static IQueryable<T> GetQuery (IQueryable<T> inpuQuery, ISpecification<T> spec)
+        {
+            if (inpuQuery != null)
+            {
+                inpuQuery = inpuQuery.Where(spec.Criteria);
+            }
+
+            inpuQuery = spec.Includes.Aggregate(inpuQuery, (current, include) => current.Include(include));
+
+            return inpuQuery;
+        }
+    }
+}
--- Core/Interfaces/IGenericRepository.cs
+++ Core/Interfaces/IGenericRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Entities;
+using Core.Specifications;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,10 @@ namespace Core.Interfaces
     {
         Task<T> GetByIdAsync(int id);
         Task<IReadOnlyList<T>> GetAllAsync();
+
+        Task<T> GetByIdWithSpec(ISpecification<T> spec);
+
+        Task<IReadOnlyList<T>> GetAllWithSpec(ISpecification<T> spec);
 
     }
 }
