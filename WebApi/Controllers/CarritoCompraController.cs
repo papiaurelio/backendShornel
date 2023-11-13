b83ec11b61ba5c8ecf2c28f1348d40aab8204400
Aurelio <byrondereyes@gmail.com>
2023-11-13 15:56:54 -0600

    agregando cambios carrito

--- WebApi/Controllers/CarritoCompraController.cs
+++ WebApi/Controllers/CarritoCompraController.cs
@@ -5,43 +5,49 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using WebApi.Extensions;
 
 namespace WebApi.Controllers
 {
     public class CarritoCompraController : BaseApiController
     {
         private readonly ICarritoCompraRepository _carritoRepository;
-        //private readonly UserManager<Usuario> _userManager;
+        private readonly UserManager<Usuario> _userManager;
         public CarritoCompraController(ICarritoCompraRepository carritoRepository,
             UserManager<Usuario> userManager)
         {
             _carritoRepository = carritoRepository;
-            //_userManager = userManager;
+            _userManager = userManager;
         }
 
         [Authorize]
         [HttpGet]
-        public async Task<ActionResult<CarritoDeCompras>> GetCarritoById(string id)
+        public async Task<ActionResult<CarritoDeCompras>> GetCarritoById()
         {
-            var carrito = await _carritoRepository.GetCarritoCompraAsync(id);
+            var usuario = await _userManager.BuscarUsuarioAsync(User);
+            
+            var carrito = await _carritoRepository.GetCarritoCompraAsync(usuario.Id);
 
-            return Ok(carrito ?? new CarritoDeCompras(id));
+            return Ok(carrito ?? new CarritoDeCompras(usuario.Id));
         }
 
         [Authorize]
         [HttpPost]
         public async Task<ActionResult<CarritoDeCompras>> UpdateCarritoCompra(CarritoDeCompras carritoParametro)
         {
-           var carritoActualizado = await _carritoRepository.UpdateCarritoDeComprasAsync(carritoParametro);
+            var usuario = await _userManager.BuscarUsuarioAsync(User);
+            carritoParametro.Id = usuario.Id;
+            var carritoActualizado = await _carritoRepository.UpdateCarritoDeComprasAsync(carritoParametro);
             return Ok(carritoActualizado);
         }
 
         [Authorize]
         [HttpDelete]
 
-        public async Task DeleteCarritoCompora(string id)
+        public async Task DeleteCarritoCompora()
         {
-           await _carritoRepository.DeleteCarritoComprasAsync(id);
+            var usuario = await _userManager.BuscarUsuarioAsync(User);
+            await _carritoRepository.DeleteCarritoComprasAsync(usuario.Id);
         }
     }
 }
